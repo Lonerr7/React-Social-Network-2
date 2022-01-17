c5ffc27a6b0fcad718a085ce148909d17b982f07
Lonerr7 <krabik2342@gmail.com>
2022-01-17 15:33:18 +0300

    Lesson 21 Dialogs layout. Created 3 names and 3 dialogs

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,7 +1,20 @@
 import s from './Dialogs.module.scss';
 
 const Dialogs = () => {
-  return <div className={s.dialogs}>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogs__items}>
+        <div className={s.dialogs__item}>Vanya:</div>
+        <div className={s.dialogs__item}>Ilya:</div>
+        <div className={s.dialogs__item}>Vadim:</div>
+      </div>
+      <div className={s.dialogs__messages}>
+        <div className={s.dialogs__message}>Hi</div>
+        <div className={s.dialogs__message}>Hello</div>
+        <div className={s.dialogs__message}>What is up</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
