c6cdb763053c0c3053e0e4941e3f10e4a441463d
Lonerr7 <krabik2342@gmail.com>
2022-02-01 09:32:07 +0300

    Lesson 93 paginator. Connected react-js-pagination plugin to make pagination

--- src/components/common/Paginator/Paginator.js
+++ src/components/common/Paginator/Paginator.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import s from './Paginator.module.scss';
+import Pagination from 'react-js-pagination';
 
 const Paginator = (props) => {
   const totalPagesCount = Math.ceil(props.totalUsersCount / props.pageLength);
 
   const pageNumbers = [];
+
   for (let i = 1; i <= totalPagesCount; i++) {
     pageNumbers.push(i);
   }
 
   return (
     <div className={s.pageNumbers__box}>
-      {pageNumbers.map((p) => (
-        <span
-          onClick={() => {
-            props.onPageChanged(p);
-          }}
-          className={props.currentPage === p ? s.active + ' ' + s.page : s.page}
-          key={p}
-        >
-          {p}
-        </span>
-      ))}
+      
+      <Pagination
+        activePage={props.currentPage}
+        itemsCountPerPage={props.pageLength}
+        totalItemsCount={totalPagesCount}
+        pageRangeDisplayed={10}
+        activeClass={s.active}
+        onChange={(pageNumber) => {
+          props.onPageChanged(pageNumber);
+        }}
+        innerClass={s.pageNumbers__box}
+      />
     </div>
   );
 };
